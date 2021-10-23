ebb6b44532d536ecc82f502feb72b54ccd0d1df1
AMITGOUD <76211905+AMITGOUD@users.noreply.github.com>
2021-10-23 12:30:10 +0530

    Add files via upload

---
+++ account1.java
@@ -0,0 +1,32 @@
+
+
+
+
+    class account1    {
+
+                            
+                          double balance=500;
+                           
+                          public void withdraw(double b){
+                                                    
+                                                    balance=balance-b;
+                                                    System.out.println("after withdraw Your account balance is"+balance);
+                                                              
+
+
+                                                       } 
+                    public void deposite(double b){
+                                                    
+                                                    balance=balance+b;
+                                                    System.out.println("after deposite Your account balance is"+balance);
+                                                              
+
+
+                                                       }                         
+
+
+
+
+
+
+                     }
